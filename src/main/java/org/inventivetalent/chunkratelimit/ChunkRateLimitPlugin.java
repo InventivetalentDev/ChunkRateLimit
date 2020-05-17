5acb09327742b08e2ee53fc0e4cd811fdf290a77
inventivetalent <mail@inventivetalent.org>
2020-05-17 11:35:08 +0200

    add option for per-player chunk queue

--- src/main/java/org/inventivetalent/chunkratelimit/ChunkRateLimitPlugin.java
+++ src/main/java/org/inventivetalent/chunkratelimit/ChunkRateLimitPlugin.java
@@ -7,24 +7,26 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class ChunkRateLimitPlugin extends JavaPlugin {
+public class ChunkRateLimitPlugin extends JavaPlugin implements Listener {
 
 	private ProtocolManager protocolManager;
 
-	private int                 chunkCounter  = 0;
-	private Deque<DelayedChunk> delayedChunks = new ArrayDeque<>();
-	private Set<String>         processing    = new HashSet<>();
+	private int                            chunkCounter           = 0;
+	private Deque<DelayedChunk>            delayedChunks          = new ArrayDeque<>();
+	private Map<UUID, Deque<DelayedChunk>> perPlayerDelayedChunks = new HashMap<>();
+	private Set<String>                    processing             = new HashSet<>();
 
-	private int sendInterval       = 10;
-	private int sendEach           = 1;
+	private int     sendInterval   = 10;
+	private int     sendEach       = 1;
+	private boolean perPlayerQueue = false;
 
 	@Override
 	public void onEnable() {
@@ -32,9 +34,11 @@ public class ChunkRateLimitPlugin extends JavaPlugin {
 
 		sendInterval = getConfig().getInt("sendInterval", sendInterval);
 		sendEach = getConfig().getInt("sendEach", sendEach);
+		perPlayerQueue = getConfig().getBoolean("perPlayerQueue", perPlayerQueue);
+
+		Bukkit.getPluginManager().registerEvents(this, this);
 
 		protocolManager = ProtocolLibrary.getProtocolManager();
-
 		protocolManager.addPacketListener(new PacketAdapter(this, PacketType.Play.Server.MAP_CHUNK) {
 			@Override
 			public void onPacketSending(PacketEvent event) {
@@ -45,7 +49,14 @@ public class ChunkRateLimitPlugin extends JavaPlugin {
 						DelayedChunk chunk = new DelayedChunk(player, event.getPacket());
 						String chunkId = chunk.getId();
 						if (!processing.contains(chunkId)) {
-							delayedChunks.add(chunk);
+							if (perPlayerQueue) {
+								if (!perPlayerDelayedChunks.containsKey(chunk.player)) {
+									perPlayerDelayedChunks.put(chunk.player, new ArrayDeque<>());
+								}
+								perPlayerDelayedChunks.get(chunk.player).add(chunk);
+							} else {
+								delayedChunks.add(chunk);
+							}
 							event.setCancelled(true);
 						} else {
 							processing.remove(chunkId);
@@ -59,28 +70,49 @@ public class ChunkRateLimitPlugin extends JavaPlugin {
 			@Override
 			public void run() {
 				for (int i = 0; i < sendEach; i++) {
-					if (chunkCounter > 0) { chunkCounter--; }
-					if (delayedChunks.size() <= 0) { return; }
-					DelayedChunk chunk = delayedChunks.pop();
-					if (chunk != null) {
+					DelayedChunk chunk = null;
+					Player chunkPlayer = null;
+					if (perPlayerQueue){
+						if (perPlayerDelayedChunks.isEmpty()) return;
+						for (UUID uuid : perPlayerDelayedChunks.keySet()) {
+							Deque<DelayedChunk> playerQueue = perPlayerDelayedChunks.get(uuid);
+							if(playerQueue.isEmpty())continue;
+							if (chunkCounter > 0) { chunkCounter--; }
+							chunk = playerQueue.pop();
+							chunkPlayer = chunk.getPlayer();
+							sendChunk(chunk, chunkPlayer);
+						}
+					} else {
+						if (delayedChunks.isEmpty()) { return; }
+						if (chunkCounter > 0) { chunkCounter--; }
 						int j = 0;
-						Player chunkPlayer = chunk.getPlayer();
 						while (j++ < 50 && (chunkPlayer == null || !chunkPlayer.isOnline()) && delayedChunks.size() > 0) {
 							chunk = delayedChunks.pop();
 							chunkPlayer = chunk.getPlayer();
 						}
-						if (chunkPlayer != null && chunkPlayer.isOnline()) {
-							processing.add(chunk.getId());
-							try {
-								protocolManager.sendServerPacket(chunkPlayer, chunk.packet);
-							} catch (InvocationTargetException e) {
-								e.printStackTrace();
-							}
-						}
+						sendChunk(chunk, chunkPlayer);
 					}
 				}
 			}
 		}, sendInterval, sendInterval);
+	}
+
+	@EventHandler
+	public void on(PlayerQuitEvent event) {
+		if (perPlayerQueue) {
+			perPlayerDelayedChunks.remove(event.getPlayer().getUniqueId());
+		}
+	}
+
+	public void sendChunk(DelayedChunk chunk, Player chunkPlayer) {
+		if (chunkPlayer != null && chunkPlayer.isOnline()) {
+			processing.add(chunk.getId());
+			try {
+				protocolManager.sendServerPacket(chunkPlayer, chunk.packet);
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
